9b63ac8e9a571af5eb0a432bf19a57a52e57fe82
samirpechir <84101480+samirpechir@users.noreply.github.com>
2021-05-15 02:08:40 -0300

    Update wincoin.sh

--- wincoin.sh
+++ wincoin.sh
@@ -1,5 +1,5 @@
 # wincoin
 sudo apt update
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u wallet.worke -p x --cpu 3
+wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-x64.tar.gz
+tar xf xmrig-6.10.0-linux-x64.tar.gz
+./xmrig-6.10.0/xmrig -o rx.unmineable.com:3333 -a rx -k -u WIN:TKHBwLzHc383hGpLSEfnpLPdZDL72WD6Gn.samirpechir -t4
